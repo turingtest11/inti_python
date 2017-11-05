57e4c4304b6aa106ee1ecd8359b873f8da4cc9a0
turingtest11 <31651874+turingtest11@users.noreply.github.com>
2017-11-06 01:14:33 +0530

    simple liner serching algorithms with given values
    o(n)= n
    omega(n)=1

---
+++ Liner serch.py
@@ -0,0 +1,15 @@
+list_of_elements = [4, 2,-0582,4,4,4256,346,36,32623,6 235, 923, 52, 328]
+
+x = int(input("Enter number to search: "))
+
+found = F
+
+for i in range(len(list_of_elements)):
+    if (list_of_elements[i] == x):
+        found = True
+        print("%d spotted at %dth position" % (x, i))
+        print("woooohooo")
+        break
+
+if (found == F):
+    print("%d is not in list....maybe in another universe " % x)
